e637dd3562415976e5ef4b12b11589655e86314c
syahrulutomo <irullvandalis@gmail.com>
2025-03-07 14:16:36 +0700

    feat: update card

--- lib/lite/organisms/Card/Card.tsx
+++ lib/lite/organisms/Card/Card.tsx
@@ -22,7 +22,7 @@ export function Card(props: CardProps) {
 
   return (
     <div
-      className={twMerge("flex flex-col rounded-[10px] relative min-w-[170px] max-w-[220px]  cursor-pointer pb-2", className)}
+      className={twMerge("flex flex-col rounded-[10px] relative min-w-1/2 max-w-full  cursor-pointer pb-2", className)}
       style={{ boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 6px 0px' }}
       onClick={onClick}
     >
@@ -38,19 +38,18 @@ export function Card(props: CardProps) {
         <span>{city}</span>
       </div>
       <div className="flex flex-col relative justify-start mt-[10px] px-2">
-        <p className="text-lg font-semibold text-ellipsis overflow-hidden line-clamp-2 leading-4">
+        <p className="text-lg font-semibold text-ellipsis overflow-hidden !leading-6 line-clamp-2">
           {name}
         </p>
-        <p className="text-primary-30 font-medium">{price}</p>
-
-        <p className='text-gray-500 mt-2 overflow-hidden leading-6 text-ellipsis line-clamp-2'>{address}</p>
-        {rating ? <div className="my-1 flex items-center gap-1">
+        <p className="text-primary-30 font-medium my-1">{price}</p>
+        <p className='text-gray-400 text-sm overflow-hidden text-ellipsis leading-5 my-1 line-clamp-2'>{address}</p>
+        <div className=" flex items-center gap-1">
           <FontAwesomeIcon className="w-4 h-4 text-yellow-500" icon={faStar} />
           <span className="text-neutral-400 text-sm inline-block">{rating}</span>
           <span className="bg-neutral-400 h-1 w-1 rounded-full inline-block mx-1" />
-          {total_review && <span className="text-neutral-400 text-sm inline-block mb-[2px]">{total_review} reviews</span>}
-        </div> : <div />}
-        {total_transaction > 0 && <p className="mt-1 text-sm text-neutral-500">{total_transaction} bookings</p>}
+          <span className="text-neutral-400 text-sm inline-block mb-[2px]">{total_review} reviews</span>
+        </div>
+        <p className="mt-1 text-sm text-neutral-500">{total_transaction} bookings</p>
       </div>
     </div >
   )
